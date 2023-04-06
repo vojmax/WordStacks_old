bec1bd18966b6889b9e83e3fef256239a3a22e37
VMaxvc <vojislav.maksovic@gmail.com>
2023-04-06 12:21:27 +0200

    Added profile part with avatar and notification components

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BsStack } from "react-icons/bs";
+import { FaBell } from "react-icons/fa";
+import Avatar from "react-avatar";
 
 import classes from "./Header.module.css";
 import Link from "next/link";
@@ -15,7 +17,18 @@ export default function Header() {
 				</Link>
 			</div>
 			<button>Add Stack</button>
-			<div>profile</div>
+			<div className={classes.profile}>
+				<button>
+					<FaBell />
+				</button>
+				<Avatar
+					name="Vanja Maskovic"
+					size="50"
+					round
+					color={"purple"}
+					onClick={null}
+				/>
+			</div>
 		</header>
 	);
 }
