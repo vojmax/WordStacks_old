df0101736778a0133dd1e86401a2b884cb774954
vojmax <vojislav.maksovic@gmail.com>
2023-04-19 17:29:40 +0200

    added env file with providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,14 @@
 import AuthLayout from "@/components/auth-layout/AuthLayout";
 import DashLayout from "@/components/dash-layout/DashLayout";
 import "@/styles/globals.css";
-import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
-	const [userLogged, setUserLogged] = useState(false);
-
 	return (
-		<div>
-			{userLogged ? (
-				<DashLayout>
-					<Component {...pageProps} />
-				</DashLayout>
-			) : (
-				<AuthLayout>
-					<Component {...pageProps} />
-				</AuthLayout>
-			)}
-		</div>
+		<SessionProvider session={pageProps.session}>
+			<AuthLayout>
+				<Component {...pageProps} />
+			</AuthLayout>
+		</SessionProvider>
 	);
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,33 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
-import GithubProvider from "next-auth/providers/github";
-import TwitterProvider from "next-auth/providers/twitter";
+import AppleProvider from "next-auth/providers/apple";
+import Email from "next-auth/providers/email";
 export const authOptions = {
 	providers: [
 		FacebookProvider({
 			clientId: process.env.FACEBOOK_ID,
 			clientSecret: process.env.FACEBOOK_SECRET,
 		}),
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
+		AppleProvider({
+			clientId: process.env.APPLE_ID,
+			clientSecret: process.env.APPLE_SECRET,
 		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
 		}),
-		TwitterProvider({
-			clientId: process.env.TWITTER_ID,
-			clientSecret: process.env.TWITTER_SECRET,
-		}),
+		// Email({
+		// 	server: {
+		// 		host: "",
+		// 		port: "",
+		// 		auth: {
+		// 			user: "",
+		// 			pass: "",
+		// 		},
+		// 		from: "",
+		// 	},
+		// }),
 	],
 	callbacks: {
 		async jwt({ token }) {
